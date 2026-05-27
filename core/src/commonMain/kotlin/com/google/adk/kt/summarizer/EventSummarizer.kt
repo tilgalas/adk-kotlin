5fc6b147b090daa5cdd437df68a258400652015e
Wiktoria Walczak <wwalczak@google.com>
2026-05-27 09:15:46 -0700

    feat: introduce EventSummarizer interface
    PiperOrigin-RevId: 922171499

---
+++ core/src/commonMain/kotlin/com/google/adk/kt/summarizer/EventSummarizer.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.adk.kt.summarizer
+
+import com.google.adk.kt.events.Event
+
+/** Base interface for producing events summary. */
+interface EventSummarizer {
+  /**
+   * Compacts the given [events] into a single [Event].
+   *
+   * Returns `null` if compaction failed. Otherwise returns a new [Event] that must carry the
+   * summary on its [com.google.adk.kt.events.EventActions.compaction] field; the original events
+   * are not modified by this call.
+   *
+   * @param events The events to summarize.
+   * @return A new compacted [Event] or `null` if no summary was produced.
+   */
+  suspend fun summarizeEvents(events: List<Event>): Event?
+}
